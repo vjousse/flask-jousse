e92f910d58ab2847f252a649a7edc847d6f858c1
Vincent Jousse <vincent@jousse.org>
2011-11-01 15:07:21 +0100

    Add admin page and login

--- jousse_blog/views/blog.py
+++ jousse_blog/views/blog.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request, \
+     session, flash, redirect, url_for
 from jinja2 import TemplateNotFound
+import websiteconfig as config
 
 mod = Blueprint('blog', __name__)
 
@@ -14,7 +16,30 @@ def index():
 
 @mod.route('/login', methods=['GET', 'POST'])
 def login():
-    try:
-        return render_template('blog/login.html')
-    except TemplateNotFound:
-        abort(404)
+    error = None
+
+    if request.method == 'POST':
+        if request.form['username'] != config.USERNAME:
+            error = 'Invalid username'
+        elif request.form['password'] != config.PASSWORD:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('.admin'))
+
+    return render_template('blog/login.html', error=error)
+
+
+@mod.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('.index'))
+
+
+@mod.route('/admin')
+def admin():
+    if(session['logged_in']):
+        return render_template('blog/admin.html')
+    else:
+        abort(401)
